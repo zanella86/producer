4e8c19f781b03d726643624b65fffd46ba14c513
Thiago <thiago_zanella@yahoo.com.br>
2022-12-04 14:41:21 -0300

    Retorno da serialização no objeto de mensagem

--- src/main/java/br/com/fiap/producer/domain/ReportStatus.java
+++ src/main/java/br/com/fiap/producer/domain/ReportStatus.java
@@ -1,11 +1,15 @@
 package br.com.fiap.producer.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.io.Serializable;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @ToString(includeFieldNames = true)
 public class ReportStatus implements Serializable {
 
